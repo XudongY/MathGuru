025cdc52f1f1d1e24f68e3d1f2f0e0640c8ac3b2
Yutong Zhao <32181647+Yutong0711@users.noreply.github.com>
2017-09-22 20:26:02 -0400

    My method of question1

--- Main.java
+++ Main.java
@@ -1,53 +1,58 @@
-package XudongYu;
-
 import java.util.Scanner;
 
 public class Main {
-    public static void question1(int a ) {
+	public static void question1(int a) {
+		// Yutong Zhao, 09-22-2017
+		double res = Math.pow(2, a);
+		String n = String.valueOf(res).substring(0,
+				(String.valueOf(res)).length() - 2);
+		if (res > 0 && res < Double.MAX_VALUE) {
+			System.out.println("2^" + a + " = " + n);
+		}
+	}
 
-    }
-    public  static  void question2(int a) {
+	public static void question2(int a) {
 
-    }
-    public static void question3(int a) {
+	}
 
-    }
+	public static void question3(int a) {
 
+	}
 
-    public static void main(String[] args) {
-        // write your code here
-        while (true) {
-            System.out.println("Please choose your option of calculation");
-            System.out.println("1 2 or 3");
-            Scanner scanner = new Scanner(System.in);
-            int option = scanner.nextInt();
-            int value;
-            while (true) {
-                System.out.println("Please input your value");
-                value = scanner.nextInt();
-                if (value > 20 || value < 0) {
-                    System.out.println("Please input value between 0 - 20");
-                } else {
-                    break;
-                }
-            }
-            switch (option) {
-                case 1:
-                    question1(value);
-                    break;
-                case 2:
-                    question2(value);
-                    break;
-                case 3:
-                    question3(value);
-                    break;
-            }
-            System.out.println("Do you want another calculation ?");
-            System.out.println("yes or no");
-            String choose = scanner.next();
-            if (choose.equals("no")) {
-                break;
-            }
-        }
-    }
+	public static void main(String[] args) {
+		// write your code here
+		while (true) {
+			System.out.println("Please choose your option of calculation");
+			System.out.println("1 2 or 3");
+			Scanner scanner = new Scanner(System.in);
+			int option = scanner.nextInt();
+			int value;
+			while (true) {
+				System.out.println("Please input your value");
+				value = scanner.nextInt();
+				if (value > 20 || value < 0) {
+					System.out.println("Please input value between 0 - 20");
+				} else {
+					break;
+				}
+			}
+			switch (option) {
+			case 1:
+				question1(value);
+				break;
+			case 2:
+				question2(value);
+				break;
+			case 3:
+				question3(value);
+				break;
+			}
+			System.out.println("Do you want another calculation ?");
+			System.out.println("yes or no");
+			String choose = scanner.next();
+			if (choose.equals("no")) {
+				break;
+			}
+		}
+	}
 }
