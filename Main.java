420a366cdf3f0204b759d089bfdea4c56a5271d2
XudongY <31521395+XudongY@users.noreply.github.com>
2017-09-24 19:13:03 -0400

    test1

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
     }
     public  static  void question2(int a) {
         System.out.println("do some test");
+        System.out.println("do some test1");
     }
 
     public static void question3(int a) {
