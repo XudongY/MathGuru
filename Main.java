0b65e60b10069f9668c1a884126fe1d3b7c00de5
XudongY <31521395+XudongY@users.noreply.github.com>
2017-09-25 10:42:58 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,8 +11,11 @@ public class Main {
     }
 
     public static void question2(int a) {
-        System.out.println("test new branch");
-        System.out.println("test new branch");
+        double res = Math.pow(2, a);
+        String result = String.valueOf(res).substring(0, (String.valueOf(res)).length() - 2);
+        if (res > 0 || res < Double.MAX_VALUE) {
+            System.out.println("2^" + a + " = " + result);
+        }
     }
 
     public static void question3(int a) {
