108277b1e818f186059a49eb6ee24d72c3466ca2
XudongY <31521395+XudongY@users.noreply.github.com>
2017-09-25 12:09:28 -0400

    disha

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ public class Main{
         }
     }
     public  static  void question2(int a) {
+        //write Disha_homework_3
         double res = Math.pow(2,a);
         String result = String.valueOf(res).substring(0, (String.valueOf(res)).length()-2);
         if(res>0 || res<Double.MAX_VALUE){
