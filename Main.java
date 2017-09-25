76d671e678ac2d5286cd4396d28d041c13b0c2a1
chenglinwu <jasonwu.601@gmail.com>
2017-09-25 16:53:01 -0400

    Chenglin Wu's Q3

--- Main.java
+++ Main.java
@@ -11,8 +11,6 @@ public class Main {
     }
     public  static  void question2(int a) {
 
-        System.out.println("test again");
-        System.out.println("test zhaoyutong");
     }
 
     public static void question3(int a) {
